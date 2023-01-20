de16136719e5a6cb886b2a15479b1a95f010b241
snaser1 <122835819+snaser1@users.noreply.github.com>
2023-01-19 23:05:05 -0500

    Add files via upload

---
+++ make_tree.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+virtual conda environments are my favorite new technology
+virtual environmemts are good for keeping dependencies seperated by project allowing you to use 
+different versions of python
+I like them because they help me organize my packages much better
+
+
+
+function greet() {
+echo 'HELLO' $1'!'
+}
+
+
